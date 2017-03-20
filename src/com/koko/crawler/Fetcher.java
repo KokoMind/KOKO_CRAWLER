3c53986ba7a6eb15b8c0c46c91aa6af14cd586ff
Mostafa Gamal <mgo_oc@hotmail.com>
2017-03-21 00:00:12 +0200

    Fetcher update1

--- src/com/koko/crawler/Fetcher.java
+++ src/com/koko/crawler/Fetcher.java
@@ -3,9 +3,14 @@ package com.koko.crawler;
 import com.koko.crawler.obj.ObjDownloaded;
 import org.jsoup.Jsoup;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 
+import com.panforge.robotstxt.RobotsTxt;
 
 public class Fetcher {
 
@@ -31,8 +36,41 @@ public class Fetcher {
     }
 
     public static boolean checkRobots(String url) {
+        try {
+            if (url.charAt(url.length() - 1) != '/')
+                url = url + '/';
+            InputStream robotsTxtStream = new URL(url + "robots.txt").openStream();
+            RobotsTxt robotsTxt = RobotsTxt.read(robotsTxtStream);
+            return robotsTxt.query("*", "/");
+        } catch (Exception e) {
             return true;
+        }
     }
+
+    public static boolean checkURL(String url) {
+        /* Checks that URL is alive and of has a response of type html. */
+        while (true) {
+            try {
+                URL urll = new URL(url);
+                HttpURLConnection connection = (HttpURLConnection) urll.openConnection();
+                connection.setRequestMethod("GET");
+                connection.connect();
+                if (connection.getResponseCode() == 200) {
+                    String contentType = connection.getHeaderField("content-type");
+                    return contentType.contains("html");
+                } else return false;
+
+            } catch (Exception e) {
+                if (Fetcher.internetOn())
+                    return false;
+                System.out.print("\rConnection lost. Retry!!");
+            }
+        }
+    }
+
+    //public static void main(String[] args) {
+        //System.out.println(checkURL("http://ilpubs.stanford.edu:8090/733/1/2002-9.pdf"));
+    //}
 
 
 }
