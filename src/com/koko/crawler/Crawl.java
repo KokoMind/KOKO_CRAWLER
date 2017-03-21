77f8740814a80c23cc4b718be8460e4cd976c424
Mostafa Gamal <mgo_oc@hotmail.com>
2017-03-21 18:45:28 +0200

    crap2

--- src/com/koko/crawler/Crawl.java
+++ src/com/koko/crawler/Crawl.java
@@ -2,47 +2,36 @@ package com.koko.crawler;
 
 import java.io.IOException;
 
-public class Crawl
-{
+public class Crawl {
 
-    public void run()
-    {
+    public void run() {
 
     }
 
-    public Crawl(String[] args) throws IOException
-    {
+    public Crawl(String[] args) throws IOException {
         if (check_args(args) == -1)
             throw new IOException();
         System.out.println("Arguments are parsed successfully and we are Ready to crawl");
     }
 
-    public int check_args(String[] args)
-    {
+    public int check_args(String[] args) {
         String firstArg;
         int secondArg;
-        if (args.length == 2)
-        {
-            try
-            {
+        if (args.length == 2) {
+            try {
                 boolean validFirstArg = false;
                 secondArg = Integer.parseInt(args[1]);
-                for (String s : moods)
-                {
+                for (String s : moods) {
                     if (s.equals(args[0]))
                         validFirstArg = true;
                 }
                 if (!validFirstArg)
                     return -1;
-            }
-            catch (NumberFormatException e)
-            {
+            } catch (NumberFormatException e) {
                 System.err.println("Argument" + args[1] + " must be an integer.");
                 return -1;
             }
-        }
-        else
-        {
+        } else {
             return -1;
         }
         return 0;
@@ -54,31 +43,31 @@ public class Crawl
     };
 
     public String seeds[] = {
-            "https://en.wikipedia.org/",
-            "http://www.W3.org/",
-            "http://web.mit.edu/",
-            "http://stanford.edu/",
-            "https://www.rottentomatoes.com/",
-            "http://www.imdb.com/",
-            "http://screenrant.com/",
-            "https://vimeo.com/",
-            "http://www.100bestwebsites.org/",
-            "http://www.makeuseof.com/tag/best-websites-internet/",
+            "https://en.wikipedia.org",
+            "http://www.W3.org",
+            "http://web.mit.edu",
+            "http://stanford.edu",
+            "https://www.rottentomatoes.com",
+            "http://www.imdb.com",
+            "http://screenrant.com",
+            "https://vimeo.com",
+            "http://www.100bestwebsites.org",
+            "http://www.makeuseof.com/tag/best-websites-internet",
             "https://moz.com/top500",
-            "https://www.bloomberg.com/",
-            "https://www.reddit.com/r/Futurology/comments/48b5oc/best_of_2015_winners/",
+            "https://www.bloomberg.com",
+            "https://www.reddit.com/r/Futurology/comments/48b5oc/best_of_2015_winners",
             "https://moz.com/blog",
-            "http://www.berkeley.edu/",
-            "https://www.cam.ac.uk/",
-            "http://www.ox.ac.uk/",
-            "http://www.caltech.edu/",
-            "http://www.dmoz.org/",
-            "http://www.ebay.com/",
-            "https://www.cnet.com/",
-            "https://www.spotify.com/",
-            "https://archive.org/",
-            "http://www.ieee.org/",
-            "http://www.nike.com/",
-            "https://en-maktoob.yahoo.com/",
+            "http://www.berkeley.edu",
+            "https://www.cam.ac.uk",
+            "http://www.ox.ac.uk",
+            "http://www.caltech.edu",
+            "http://www.dmoz.org",
+            "http://www.ebay.com",
+            "https://www.cnet.com",
+            "https://www.spotify.com",
+            "https://archive.org",
+            "http://www.ieee.org",
+            "http://www.nike.com",
+            "https://en-maktoob.yahoo.com",
     };
 }
--- src/com/koko/crawler/Fetcher.java
+++ src/com/koko/crawler/Fetcher.java
@@ -2,23 +2,23 @@ package com.koko.crawler;
 
 import com.koko.crawler.obj.ObjDownloaded;
 
-import java.io.File;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 import java.net.*;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
 
+import com.koko.crawler.obj.ObjLink;
 import com.panforge.robotstxt.RobotsTxt;
-import org.apache.commons.io.FilenameUtils;
+import com.shekhargulati.urlcleaner.UrlCleaner;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class Fetcher {
     /* Class that will fetch the page, validate that it is of type HTML, extract its contents and hyperlinks */
 
-    public static ObjDownloaded fetch(String url) {
-        return null;
-    }
-
-    public static String downloadPage(String url) {
-        return "";
-    }
 
     public static boolean internetOn() {
         for (int i = 0; i < 5; i++) {
@@ -79,12 +79,76 @@ public class Fetcher {
     public static boolean checkExtHTML(String url) {
         /* check the filename extension either HTML or "" */
         //Implementations in Java are pieces of crap.
-        return false;
+        String sep = "\\", altsep = "/", extsep = ".";
+        int sepIndex = url.lastIndexOf(sep);
+        int altsepIndex = url.lastIndexOf(altsep);
+        sepIndex = Math.max(sepIndex, altsepIndex);
+
+        int dotIndex = url.lastIndexOf(extsep);
+        if (dotIndex > sepIndex) {   //skip all leading dots
+            int filenameIndex = sepIndex + 1;
+            while (filenameIndex < dotIndex) {
+                if (!url.substring(filenameIndex, filenameIndex + 1).equals(extsep))
+                    return url[:dotIndex],p[dotIndex:];
+                filenameIndex += 1;
+            }
+            return false;
+        }
+
+    public static Document downloadPage(String url) {
+        if (Fetcher.checkURL(url) && Fetcher.checkRobots(url)) {
+            try {
+                return Jsoup.connect(url).get();
+            } catch (Exception e) {
+                return null;
+            }
+        }
+        return null;
     }
 
-    //public static void main(String[] args) {
-      //  System.out.println(checkExtHTML("http://ilpubs.stanford.edu"));
-    //}
+    private static String extractASCIIOnly(String data) throws UnsupportedEncodingException {
+        data = new String(Charset.forName("ascii").encode(data).array(), "ascii");
+        data = data.replace('?', ' ');
+        return data;
+    }
+
+    public static ObjDownloaded fetch(String url) {
+        Document soup = Fetcher.downloadPage(url);
+        return null;
+    }
+
+    public static String testfetch(String url) {
+        try {
+            Document soup = Fetcher.downloadPage(url);
+            String content = extractContent(soup);
+            ObjLink[] links = null;
+            links = extractLinks(soup).toArray(links);
+            return null;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    private static ArrayList<ObjLink> extractLinks(Document soup) {
+        Elements links = soup.select("a");
+        ArrayList<ObjLink> stringLinks = new ArrayList<>();
+        for (Element link : links) {
+            String absHref = link.attr("abs:href"); // "http://jsoup.org/
+            String normalizedURL = UrlCleaner.normalizeUrl(absHref);
+            if (Fetcher.checkExtHTML(normalizedURL))
+                stringLinks.add(new ObjLink(normalizedURL, Fetcher.extractDNS(normalizedURL)));
+        }
+        return stringLinks;
+    }
+
+
+    public static String extractContent(Document soup) throws UnsupportedEncodingException {
+        return extractASCIIOnly(soup.text()).trim().replaceAll(" +", " ");
+    }
+
+    public static void main(String[] args) {
+        testfetch("https://en.wikipedia.org/wiki/URL_normalization");
+    }
 
 
 }
