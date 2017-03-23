aea7e96d848e50bbc53b012a2202fdeb9c3ef568
Mo'men AbdelRazek <runmoemen@gmail.com>
2017-03-23 19:11:09 +0200

    Obj Page

---
+++ src/ObjPage.java
@@ -0,0 +1,15 @@
+public class ObjPage
+{
+    String url;
+    String dns;
+    String content;
+    int id;
+
+    public ObjPage(String url, String dns, String content, int id)
+    {
+        this.url = url;
+        this.dns = dns;
+        this.content = content;
+        this.id = id;
+    }
+}
