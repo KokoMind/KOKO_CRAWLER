025811e4c1ee3ce9fefbd37b5576e78b51fe0076
Mo'men AbdelRazek <runmoemen@gmail.com>
2017-03-24 15:48:25 +0200

    ObjPAge

--- src/ObjPage.java
+++ src/ObjPage.java
@@ -1,15 +1,19 @@
+import org.bson.Document;
+
+import java.time.LocalDateTime;
+
 public class ObjPage
 {
-    String url;
-    String dns;
-    String content;
-    int id;
+    Document record;
 
-    public ObjPage(String url, String dns, String content, int id)
+    public ObjPage(String url, String dns, String content, int thread_id)
     {
-        this.url = url;
-        this.dns = dns;
-        this.content = content;
-        this.id = id;
+        record = new Document();
+        record.put("url", url);
+        record.put("dns", dns);
+        record.put("thread_id", thread_id);
+        record.put("visited", LocalDateTime.now().toString());
+        record.put("indexed", "false");
+        record.put("content", content);
     }
 }
