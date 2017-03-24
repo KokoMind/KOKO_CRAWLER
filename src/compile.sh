991e73bcf2186397ba39dbd199219f7566344890
Mo'men AbdelRazek <runmoemen@gmail.com>
2017-03-24 17:53:41 +0200

    New libraries added

--- src/compile.sh
+++ src/compile.sh
@@ -7,6 +7,6 @@ echo "YARAB"
 
 rm classes/*.class
 
-javac -cp ".:1.jar:2.jar:3.jar:4.jar:5.jar:6.jar:7.jar:8.jar:9.jar" *.java -d classes
+javac -cp ".:1.jar:2.jar:3.jar:4.jar:5.jar:6.jar:7.jar:8.jar:9.jar:10.jar:11.jar:12.jar" *.java -d classes
 
 echo "DONE"
